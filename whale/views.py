09637dda6c243e260d9d4eba706530ba540e8ad9
John Wall <john.wall@noaa.gov>
2024-10-01 13:55:47 -0400

    Organize libraries

--- whale/views.py
+++ whale/views.py
@@ -1,52 +1,60 @@
+# Basic stack
 import os
-import hmac
-import hashlib
-import base64
-from time import time
-from glob import glob
+import sys
 import json
 import requests
 import datetime
+from datetime import datetime, timedelta
+import subprocess
+from time import time
+from glob import glob
+from wsgiref.util import FileWrapper
+
+# Security stack
+import hmac
+import hashlib
+import base64
+
+# Geospatial Stack
+from pyproj import CRS, Transformer
+from osgeo import gdal
+from osgeo_utils.gdal_pansharpen import gdal_pansharpen
 from shapely import to_geojson
 from shapely.geometry import box, Point, Polygon
 from fiona.drvsupport import supported_drivers
 import pandas as pd
 import geopandas as gpd
-from osgeo import gdal
-from osgeo_utils.gdal_pansharpen import gdal_pansharpen
-from pyproj import CRS, Transformer
-import subprocess
-import sys
+
+# Azure stack
+from azure.core.credentials import AzureNamedKeyCredential
 from azure.storage.blob import BlobServiceClient, generate_blob_sas, BlobSasPermissions
-from django.urls import reverse
-from django.db import IntegrityError
-from django.db.models import Q
-from django.conf import settings
-from django.contrib import messages
-from django.contrib.auth import authenticate, login
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.gis.geos import GEOSGeometry
+
+# Django stack
 from django.core.cache import cache
 from django.core.serializers import serialize
 from django.core.serializers.json import DjangoJSONEncoder
-from django.utils.safestring import mark_safe
-from django.http import JsonResponse, HttpResponse, HttpResponseForbidden
-from django.shortcuts import render, redirect, get_object_or_404
+from django.conf import settings
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
 from django.contrib.gis.geos import GEOSGeometry
 from django.contrib.gis.db.models.functions import Transform
 from django.contrib.sessions.models import Session
-from django.views import View
+from django.db import IntegrityError
+from django.db.models import Q
+from django.http import JsonResponse, HttpResponse, HttpResponseForbidden
+from django.urls import reverse
+from django.utils.safestring import mark_safe
+from django.shortcuts import render, redirect, get_object_or_404
 from django_q.tasks import async_task
-from wsgiref.util import FileWrapper
-from azure.storage.blob import generate_blob_sas, BlobSasPermissions
-from azure.core.credentials import AzureNamedKeyCredential
-from datetime import datetime, timedelta
-from .models import AreaOfInterest, EarthExplorer, GEOINTDiscovery, MaxarGeospatialPlatform, ExtractTransformLoad
-from .models import PointsOfInterest
+from django.views import View
+
+# GAIA stack
+from .security import ee_login, mgp_login
+from .models import AreaOfInterest, PointsOfInterest, EarthExplorer, GEOINTDiscovery, MaxarGeospatialPlatform, ExtractTransformLoad
 from .forms import APIQueryForm, ProcessingForm, PointsOfInterestForm
 from .tasks import process_etl_data
-from .security import ee_login, mgp_login
 from .query import build_ee_query_payload, query_mgp
 from .download import download_imagery
 from .utils import standardize_names, calibrate_image, convert_to_tiles, get_entity_pairs, import_pois
