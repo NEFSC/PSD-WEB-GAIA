e05ad97be8b57ba09247a2f83eb3224c83d43fc2
Marcus England <marcus.england@noaa.gov>
2025-02-04 15:02:10 -0700

    Working fix

--- whale/views.py
+++ whale/views.py
@@ -69,22 +69,6 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gaia.settings')
 os.environ["CPL_DEBUG"] = "ON" # Should enable GDAL debuggin
 django.setup()
 
-def login_view(request):
-    """ A simple log-in page meeting NOAA OCIO's security requirement for
-            a username and password protecting restricted access
-            satellite imagery.
-    """
-    if request.method == 'POST':
-        form = AuthenticationForm(request, data=request.POST)
-        if form.is_valid():
-            user = form.get_user()
-            login(request, user)
-            return redirect('landing_page')
-    else:
-        form = AuthenticationForm()
-        
-    return render(request, 'login.html', {'form': form})
-
 def landing_page(request):
     """ A basic landing page for the WHale Active Learning Environment (WHALE)
             Tasking, Collection, Processing, Exploitation, and Dissimination
