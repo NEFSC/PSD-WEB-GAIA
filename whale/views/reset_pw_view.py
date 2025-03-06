3f3afc2fa45de9299ee083e86065f47163e3dfa4
Marcus England <marcus.england@noaa.gov>
2025-03-06 14:21:16 -0700

    Initial commit

---
+++ whale/views/reset_pw_view.py
@@ -0,0 +1,13 @@
+from django.urls import reverse_lazy
+from django.contrib.auth.views import PasswordResetView
+from django.contrib.messages.views import SuccessMessageMixin
+
+class MyResetPasswordView(SuccessMessageMixin, PasswordResetView):
+    template_name = 'password_reset/password_reset_form.html'
+    email_template_name = 'password_reset/registration/password_reset_email.html'
+    subject_template_name = 'password_reset/registration/password_reset_subject'
+    success_message = "We've emailed you instructions for setting your password, " \
+                      "if an account exists with the email you entered. You should receive them shortly." \
+                      " If you don't receive an email, " \
+                      "please make sure you've entered the address you registered with, and check your spam folder."
+    success_url = reverse_lazy('users-home')
