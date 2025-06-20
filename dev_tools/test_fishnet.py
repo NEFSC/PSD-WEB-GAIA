542c4a2d0fe1861f3be1aad9100bec9e632ee878
John Wall <john.wall@noaa.gov>
2025-06-20 16:15:44 -0400

    Add hypothetical fishnet commit to database

--- dev_tools/test_fishnet.py
+++ dev_tools/test_fishnet.py
@@ -11,13 +11,17 @@
 
 
 # ------------------------------------------------------------------------------
-# Import libraries
+# Import libraries, configure environment
 # ------------------------------------------------------------------------------
 import os
 import sys
+import django
+import asyncio
 import importlib
+from time import time
 from glob import glob
 from pathlib import Path
+from asgiref.sync import sync_to_async
 
 project_dir = "../../../gis/PSD-WEB-GAIA"
 project_dir = os.path.abspath(project_dir)
@@ -25,6 +29,12 @@ sys.path.append(str(project_dir))
 
 import utils.spatial_ops
 importlib.reload(utils.spatial_ops)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'gaia.settings'
+django.setup()
+
+from animal.models import Fishnet as FN
+
 
 # ------------------------------------------------------------------------------
 # Basic test for fishnet method
@@ -38,3 +48,35 @@ cogs = glob(os.path.join(data_dir, sub_dir, '**', "*.tif"), recursive=True)
 test_cog = [cogs[0]]
 print(f"\nRunning test on {test_cog[0]}!\n")
 fishnet_gdf = utils.spatial_ops.create_fishnet(test_cog)
+
+
+# ------------------------------------------------------------------------------
+# Import fishnet into the SpatiaLite database
+# ------------------------------------------------------------------------------
+def import_fishnet(gdf):
+    """ Synchronous Import fishnet cells function.
+
+        When provided with a GeoDataFrame of cells representing a fishnet, load
+            them into the SpatiaLite database.
+
+        GDF - A GeoDataFrame of fishnet cells with vendor id and geometry
+            attributes.
+    """
+    for index, row in gdf.iterrows():
+        fn, created = FN.objects.create(
+            'vendor_id': row['vendor_id'],
+            'cell': row['geometry'].wkt
+        )
+
+async def import_fishnet_async(gdf):
+    await sync_to_async(import_fishnet, thread_sensitive=True)(gdf)
+
+start = time()
+
+async def run_all():
+    await import_fishnet_async(fishnet_gdf)
+
+asyncio.run(run_all())
+
+end = time()
+print(f"\n Loaded in {round(end - start, 2)} seconds.")
